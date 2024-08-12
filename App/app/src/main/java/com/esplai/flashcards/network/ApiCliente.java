14def94f122cb05f6ce5b056e5985dc8c32440c5
jiajun10901232 <115166807+JiajunQiuWu@users.noreply.github.com>
2024-08-12 14:00:07 +0200

    limitacion de peticion al registrar

--- App/app/src/main/java/com/esplai/flashcards/network/ApiCliente.java
+++ App/app/src/main/java/com/esplai/flashcards/network/ApiCliente.java
@@ -1,6 +1,8 @@
 package com.esplai.flashcards.network;
 
 
+import java.net.URL;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,11 +10,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 //llamada a la api
 public class ApiCliente {
     private static Retrofit retrofit = null;
-
+    private static final String url="http://10.0.2.2:8181/";
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://10.0.2.2:8181/")   // URL base de tu API
+                    .baseUrl(url)  // URL base de tu API
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
