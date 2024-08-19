7966d1bc79dcfd2f4b040e5c1e08ed7bdcd58057
AlexFlFe <alexfloresfelguera@gmail.com>
2024-08-19 12:37:16 +0200

    minor fixes

--- App/app/src/main/java/com/esplai/flashcards/network/ApiCliente.java
+++ App/app/src/main/java/com/esplai/flashcards/network/ApiCliente.java
@@ -8,7 +8,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 //llamada a la api
 public class ApiCliente {
     private static Retrofit retrofit = null;
-    private static final String url="http://10.0.2.2:8181/";
+    private static final String url="http://10.0.2.2:8182/";
     public static Retrofit getClient() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
