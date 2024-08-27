7b5bdae327ee10ec696acb7fd0540a63d5ff0015
AlexFlFe <alexfloresfelguera@gmail.com>
2024-08-27 19:16:03 +0200

    added AWS route

--- App/app/src/main/java/com/esplai/flashcards/network/ApiCliente.java
+++ App/app/src/main/java/com/esplai/flashcards/network/ApiCliente.java
@@ -14,7 +14,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 //llamada a la api
 public class ApiCliente {
     private static Retrofit retrofit = null;
-    private static final String url="http://10.0.2.2:8182/";
+    private static final String url="http://ec2-13-60-89-230.eu-north-1.compute.amazonaws.com:8182/";
     public static Retrofit getClient() {
         if (retrofit == null) {
             Gson gson = new GsonBuilder()
